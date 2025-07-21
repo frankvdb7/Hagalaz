9ae21d9e3c7ec89b2505edbfc670ee98ed6b6c68
frankvdb7 <5363672+frankvdb7@users.noreply.github.com>
2025-07-22 00:28:24 +0200

    chore: Update OpenIddict token type column length to 150 and adjust related migrations

---
+++ Hagalaz.Data/Migrations/20250721222703_UpdateOpenIddict7.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hagalaz.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOpenIddict7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: true,
+                collation: "utf8mb4_0900_ai_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                collation: "utf8mb4_0900_ai_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+        }
+    }
+}
